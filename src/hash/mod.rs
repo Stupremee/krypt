21e6fc34cb3a8269ea54b637bef9551edd72017f
Justus K <justus.k@protonmail.com>
2019-12-23 17:55:57 +0100

    Move generic.rs contents to hash/mod.rs

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -1,6 +1,4 @@
-pub mod generic;
-
-use generic::*;
+use digest::Digest;
 
 pub(crate) const ALGORITHMS: [(&str, &dyn HashAlgorithm); 20] = [
     ("blake2b", &Blake2bHasher {}),
@@ -27,6 +25,44 @@ pub(crate) const ALGORITHMS: [(&str, &dyn HashAlgorithm); 20] = [
 
 pub trait HashAlgorithm {
     fn hash(&self, data: Vec<u8>) -> Vec<u8>;
+}
+
+macro_rules! generic_algorithm {
+    ($arg:path, $name:ident) => {
+        pub struct $name;
+        impl $crate::hash::HashAlgorithm for $name {
+            fn hash(&self, data: Vec<u8>) -> Vec<u8> {
+                generic_hash::<$arg>(data)
+            }
+        }
+    };
+}
+
+generic_algorithm!(blake2::Blake2b, Blake2bHasher);
+generic_algorithm!(blake2::Blake2s, Blake2sHasher);
+generic_algorithm!(md2::Md2, Md2Hasher);
+generic_algorithm!(md4::Md4, Md4Hasher);
+generic_algorithm!(md5::Md5, Md5Hasher);
+generic_algorithm!(sha1::Sha1, Sha1Hasher);
+generic_algorithm!(sha2::Sha224, Sha224Hasher);
+generic_algorithm!(sha2::Sha256, Sha256Hasher);
+generic_algorithm!(sha2::Sha384, Sha384Hasher);
+generic_algorithm!(sha2::Sha512, Sha512Hasher);
+generic_algorithm!(sha3::Keccak224, Keccak224Hasher);
+generic_algorithm!(sha3::Keccak256, Keccak256Hasher);
+generic_algorithm!(sha3::Keccak384, Keccak384Hasher);
+generic_algorithm!(sha3::Keccak512, Keccak512Hasher);
+generic_algorithm!(sha3::Sha3_224, Sha3_224Hasher);
+generic_algorithm!(sha3::Sha3_256, Sha3_256Hasher);
+generic_algorithm!(sha3::Sha3_384, Sha3_384Hasher);
+generic_algorithm!(sha3::Sha3_512, Sha3_512Hasher);
+generic_algorithm!(streebog::Streebog256, Streebog256Hasher);
+generic_algorithm!(streebog::Streebog512, Streebog512Hasher);
+
+fn generic_hash<D: Digest>(data: Vec<u8>) -> Vec<u8> {
+    let mut hasher = D::new();
+    hasher.input(data);
+    hasher.result().into_iter().collect::<Vec<u8>>()
 }
 
 pub fn find_hash_for_name(name: &str) -> Option<&dyn HashAlgorithm> {
