fedbd87f7b68a4b5481accc0d62185e59b9a9ea4
Justus K <justus.k@protonmail.com>
2020-06-12 16:20:32 +0200

    Include Cargo.lock and add HashAlgorithm enum

---
+++ src/hash.rs
@@ -0,0 +1,42 @@
+//! Contains all stuff that is required for the Hash mode operations.
+
+/// All hash algorithms that are supported by krypt.
+#[derive(Debug)]
+pub enum HashAlgorithm {
+    Blake2b,
+    Blake2s,
+
+    Blake3,
+
+    Md2,
+    Md4,
+    Md5,
+
+    Sha1,
+
+    Sha224,
+    Sha256,
+    Sha384,
+    Sha512,
+
+    Sha3_224,
+    Sha3_256,
+    Sha3_384,
+    Sha3_512,
+
+    Keccak224,
+    Keccak256,
+    Keccak384,
+    Keccak512,
+
+    Shabal192,
+    Shabal224,
+    Shabal256,
+    Shabal384,
+    Shabal512,
+
+    Streebog256,
+    Streebog512,
+
+    Whirlpool,
+}
